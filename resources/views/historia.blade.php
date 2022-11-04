3aa25949407c08f92a17e60972a15d4fbdcee7da
rolen-nolberto <1376509@senati.pe>
2022-11-04 17:03:58 -0500

    cambios

---
+++ resources/views/historia.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>HOLA MUNDO</h1>
+    <form action="accion.php" method="post">
+ <p>Su nombre: <input type="text" name="nombre" /></p>
+ <p>Su edad: <input type="text" name="edad" /></p>
+ <p><input type="submit" /></p>
+</form>
+
+</body>
+</html>
